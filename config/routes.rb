291eba84e173aad4cc0544b4edd401012a2fcd8a
christine <christinejaouen@hotmail.com>
2018-11-01 13:43:29 +0100

    ve boot

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   resources :twitts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post '/user', to: 'users#new'
+  get '/static_page/home', to: 'static_page#home'
+  get '/user/create', to: 'users#create'
+  get '/user/new', to: 'users#new'
+  get '/private_message/new', to: 'private_messages#new'
+  get '/private_message/create', to: 'private_messages#create'
+
 end
